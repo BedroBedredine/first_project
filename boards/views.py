1412ecbd2889a3ad38527ccb7b566732dbd080d2
BedroBedredine <bedroghanoubedredine@gmail.com>
2020-09-27 22:16:56 +0100

    Views

--- boards/views.py
+++ boards/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Board
 # Create your views here.
 
 
 def home(request):
+    boards = Board.objects.all()
+    boards_names = []
+    for board in boards:
+        boards_names.append(board.name)
+    print(boards_names)
 
-    return HttpResponse("Hello, My name is Bedro")
+
+    response_html = '<br>'.join(boards_names)
+    return HttpResponse(response_html)
