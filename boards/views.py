16a282fb07ceb60f5562abbcd55743c8f779130f
BedroBedredine <bedroghanoubedredine@gmail.com>
2020-10-01 19:30:30 +0100

    Django Forms

--- boards/views.py
+++ boards/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse,Http404
 from django.contrib.auth.models import User
 from .models import Board
 from .models import Topic,Post
+from .forms import NewTopicForm
 
 # Create your views here.
 
@@ -21,20 +22,20 @@ def board_topics(request,board_id):
 
 def new_topic(request,board_id):
     board = get_object_or_404(Board,pk=board_id)
-    if request.method == 'POST':
-        subject = request.POST['subject']
-        message = request.POST['message']
-        user = User.objects.first()
-
-        topic = Topic.objects.create(
-            subject=subject,
-            board=board,
-            created_by=user
-        )
-        post = Post.objects.create(
-            message=message,
-            topic=topic,
-            created_by=user
-        )
-        return redirect('board_topics', board_id=board.pk)
-    return render(request,'new_topic.html',{'board':board})
+    user = User.objects.first()
+    if request.method == "POST":
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.created_by = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                created_by = user,
+                topic=topic
+            )
+            return redirect('board_topics',board_id=board.pk)
+    else:
+        form = NewTopicForm()
+    return render(request,'new_topic.html',{'board':board,'form':form})
