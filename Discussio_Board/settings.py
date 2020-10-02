d75d114d9132e7acc5809643584be475645d3991
BedroBedredine <bedroghanoubedredine@gmail.com>
2020-10-02 16:52:22 +0100

    Authentication

--- Discussio_Board/settings.py
+++ Discussio_Board/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'widget_tweaks',
+    'widget_tweaks',        
     'boards',
+    'accounts',
 ]
 
 MIDDLEWARE = [
