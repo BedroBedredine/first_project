69b326098906748776f84610774218e63da31345
BedroBedredine <bedroghanoubedredine@gmail.com>
2020-10-12 22:11:54 +0100

    My account

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login as auth_login
 from .forms import SignUpForm
-
+from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+from django.shortcuts import render,redirect
+from django.views.generic import UpdateView
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def signup(request):
@@ -15,3 +17,12 @@ def signup(request):
             return redirect('home')
 
     return render(request,'signup.html',{'form':form})
+
+class UserUpdateView(UpdateView):
+    model = User
+    fields = ('first_name','last_name','email',)
+    template_name = 'my_account.html'
+    success_url = reverse_lazy('my_account')
+    
+    def get_object(self):
+        return self.request.user
